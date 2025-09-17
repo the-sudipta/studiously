12e9651176e35494634d12d29736b37fbaa840b8
Sudipta Desktop BD <160587586+the-sudipta@users.noreply.github.com>
2025-09-17 06:31:42 +0600

    🛠 config: Configure TypeORM module in Studiously
    Set up TypeOrmModule integration in the Studiously project, including database connection parameters, entity registration, and synchronization options. Ensures smooth ORM functionality for managing schema, repositories, and migrations.

--- studiously/src/app.module.ts
+++ studiously/src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+// import { AuthModule } from './customer/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT!, 10),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILER_HOST,
+        port: parseInt(process.env.MAILER_PORT!, 10),
+        ignoreTLS: true,
+        secure: true,
+        auth: {
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASSWORD,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
